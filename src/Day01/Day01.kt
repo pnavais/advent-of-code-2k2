8b33dd7f1898e640280e01c19cdcdfffaca054e4
Pablo Navais <pnavais@gmail.com>
2022-12-07 19:20:29 +0100

    Refactored code and added gradle specific properties

--- src/Day01/Day01.kt
+++ src/Day01/Day01.kt
@@ -1,48 +1,47 @@
+fun part1(input: List<String>): Pair<Long, Long> {
+
+    var maxElf = 1L
+    var currentElf = 1L
+    var currentSum = 0L
+    var maxSum = -1L
+
+    for (value in input) {
+        if (value.isNotEmpty()) {
+            currentSum += value.toLong()
+        } else {
+            if (currentSum > maxSum) {
+                maxSum = currentSum
+                maxElf = currentElf
+            }
+            currentSum = 0
+            currentElf++
+        }
+    }
+
+    return maxElf to maxSum
+}
+
+fun part2(input: List<String>): Long {
+
+    var currentElf = 1L
+    var currentSum = 0L
+
+    val totalList = mutableListOf<Long>();
+
+    for (value in input) {
+        if (value.isNotEmpty()) {
+            currentSum += value.toLong()
+        } else {
+            totalList.add(currentSum)
+            currentSum = 0
+            currentElf++
+        }
+    }
+
+    return totalList.sortedDescending().toMutableList().subList(0, 3).sum()
+}
+
 fun main() {
-
-    fun part1(input: List<String>): Pair<Long, Long> {
-
-        var maxElf = 1L
-        var currentElf = 1L
-        var currentSum = 0L
-        var maxSum = -1L
-
-        for (value in input) {
-            if (value.isNotEmpty()) {
-                currentSum += value.toLong()
-            } else {
-                if (currentSum > maxSum) {
-                    maxSum = currentSum
-                    maxElf = currentElf
-                }
-                currentSum = 0
-                currentElf++
-            }
-        }
-
-        return maxElf to maxSum
-    }
-
-    fun part2(input: List<String>): Long {
-
-        var currentElf = 1L
-        var currentSum = 0L
-
-        val totalList = mutableListOf<Long>();
-
-        for (value in input) {
-            if (value.isNotEmpty()) {
-                currentSum += value.toLong()
-            } else {
-                totalList.add(currentSum)
-                currentSum = 0
-                currentElf++
-            }
-        }
-
-        return totalList.sortedDescending().toMutableList().subList(0,3).sum()
-    }
-
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01/Day01_test")
     println(part1(testInput))
--- src/day03/Day03.kt
+++ src/day03/Day03.kt
@@ -2,45 +2,59 @@ package day03
 
 import readInput
 
-fun main() {
-
-    fun computePriority(item : Char) : Short {
-        var itemId = item.code.toShort()
-        itemId = if (itemId >= 'a'.code.toShort() && itemId <= 'z'.code.toShort()) {
-            itemId.minus('a'.code.toShort()-1).toShort()
-        } else if (itemId >= 'A'.code.toShort() && itemId <= 'Z'.code.toShort()) {
-            itemId.minus('A'.code.toShort()-27).toShort()
-        } else {
-            0
-        }
-
-        return itemId
+fun computePriority(item: Char): Short {
+    var itemId = item.code.toShort()
+    itemId = if (itemId >= 'a'.code.toShort() && itemId <= 'z'.code.toShort()) {
+        itemId.minus('a'.code.toShort() - 1).toShort()
+    } else if (itemId >= 'A'.code.toShort() && itemId <= 'Z'.code.toShort()) {
+        itemId.minus('A'.code.toShort() - 27).toShort()
+    } else {
+        0
     }
 
-    fun part1(input: List<String>) : Long {
-        var totalPriorities = 0L
+    return itemId
+}
 
-        for (ruckSackSpec in input) {
-            val firstRuckSack = ruckSackSpec.substring(0, ruckSackSpec.length/2)
-            val secondRuckSack = ruckSackSpec.substring(ruckSackSpec.length/2)
-            var commonItem = Character.MIN_VALUE
-            for (item in secondRuckSack.toCharArray()) {
-                if (firstRuckSack.indexOf(item) != -1) {
-                    commonItem = item
-                    break
-                }
+fun part1(input: List<String>): Long {
+    var totalPriorities = 0L
+
+    for (ruckSackSpec in input) {
+        val firstRuckSack = ruckSackSpec.substring(0, ruckSackSpec.length / 2)
+        val secondRuckSack = ruckSackSpec.substring(ruckSackSpec.length / 2)
+        var commonItem = Character.MIN_VALUE
+        for (item in secondRuckSack.toCharArray()) {
+            if (firstRuckSack.indexOf(item) != -1) {
+                commonItem = item
+                break
             }
-            val priority = computePriority(commonItem)
-            totalPriorities+=priority
         }
-
-        return totalPriorities
+        totalPriorities += computePriority(commonItem)
     }
 
-    fun part2(input: List<String>): Long {
-        return 0L
+    return totalPriorities
+}
+
+fun part2(input: List<String>): Long {
+    var totalPriorities = 0L
+
+    for (i in input.indices step 3) {
+        var commonItem = Character.MIN_VALUE
+        val firstRuckSack = input[i]
+        val secondRuckSack = input[i + 1]
+        val thirdRuckSack = input[i + 2]
+        for (item in firstRuckSack.toCharArray()) {
+            if ((secondRuckSack.indexOf(item) != -1) && (thirdRuckSack.indexOf(item) != -1)) {
+                commonItem = item
+                break
+            }
+        }
+        totalPriorities += computePriority(commonItem)
     }
 
+    return totalPriorities
+}
+
+fun main() {
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("day03/Day03_test")
     println(part1(testInput))
