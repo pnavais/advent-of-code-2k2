9e4f1b092d9ee41939a062b331a0fa21e8f5da71
Pablo Navais <pnavais@gmail.com>
2022-12-14 00:19:30 +0100

    Added Day 13 solution (First half)

--- src/day13/Day13.kt
+++ src/day13/Day13.kt
@@ -3,6 +3,8 @@ package day13
 import readInput
 import java.util.Stack
 
+typealias PacketPair = Pair<Packet, Packet>
+
 interface Value {
     fun getData(): Any
 }
@@ -16,28 +18,128 @@ class IntValue(private val data: Int) : Value {
             return IntValue(s.toInt())
         }
     }
+
+    override fun toString(): String {
+        return getData().toString()
+    }
 }
 
 class ListValue : Value {
     private var listValue = mutableListOf<Value>()
 
-    override fun getData(): Any {
+    override fun getData(): MutableList<Value> {
         return listValue
     }
 
     fun add(v: Value) {
         listValue.add(v)
     }
-}
 
-fun readSpec(input: List<String>): List<ListValue> {
-    val listsValue = mutableListOf<ListValue>()
-    for (line in input) {
-        if (line.isNotEmpty()) {
-            listsValue.add(processList(line))
+    companion object {
+        fun from(value: IntValue): ListValue {
+            val l = ListValue()
+            l.add(value)
+            return l
         }
     }
-    return listsValue
+
+    override fun toString(): String {
+        val builder = StringBuilder()
+        builder.append("[")
+        var prefix = ""
+        for (item in listValue) {
+            builder.append(prefix)
+            builder.append(item.toString())
+            prefix = ","
+        }
+        builder.append("]")
+        return builder.toString()
+    }
+}
+
+data class Packet(val list: ListValue)
+
+class PacketList {
+    val packetPairs = mutableListOf<PacketPair>()
+
+    fun process(): Int {
+        var sum = 0
+        for ((i, pair) in packetPairs.withIndex()) {
+            val result = processPair(pair)
+            if ((result == null) || (result)) {
+                sum+=(i+1)
+            }
+        }
+        return sum
+    }
+
+    private fun processPair(pair: PacketPair): Boolean? {
+        return compareListValues(pair.first.list, pair.second.list)
+    }
+
+    private fun compareListValues(firstList: ListValue, secondList: ListValue): Boolean? {
+        var result: Boolean? = null
+        for ((i, item) in firstList.getData().withIndex()) {
+            result = if (secondList.getData().size > i) {
+                compareValues(item, secondList.getData()[i])
+            } else {
+                false
+            }
+            if (result != null) {
+                break
+            }
+        }
+
+        if ((result == null) && (firstList.getData().size < secondList.getData().size)) {
+            result = true
+        }
+
+        return result
+    }
+
+    private fun compareValues(first: Value, second: Value): Boolean? {
+        var res: Boolean? = null
+        var firstValue: Value = first
+        var compareLists = true
+
+        if (first is IntValue) {
+            if (second is IntValue) {
+                if ((first.getData() != second.getData())) {
+                    res = (first.getData() < second.getData())
+                }
+                compareLists = false
+            } else {
+                firstValue = ListValue.from(first)
+            }
+        }
+
+        if (compareLists) {
+            val secondValue: ListValue = if (second is ListValue) { second } else {
+                ListValue.from(second as IntValue)
+            }
+            res = compareListValues(firstValue as ListValue, secondValue)
+        }
+
+        return res
+    }
+}
+
+fun readSpec(input: List<String>): PacketList {
+    val packetList = PacketList()
+    var firstPacket: Packet? = null
+    for (line in input) {
+        if (line.isNotEmpty()) {
+            val newPacket = Packet(processList(line))
+            firstPacket = if (firstPacket == null) {
+                newPacket
+            } else {
+                packetList.packetPairs.add(firstPacket to newPacket)
+                null
+            }
+
+        }
+    }
+    return packetList
 }
 
 private fun processList(line: String): ListValue {
@@ -46,7 +148,7 @@ private fun processList(line: String): ListValue {
     val digitBuilder = StringBuilder()
     for (c in line.toCharArray()) {
         if (c == '[') {
-            if (currentList!=null) {
+            if (currentList != null) {
                 listStack.push(currentList)
             }
             currentList = ListValue()
@@ -73,8 +175,8 @@ private fun processList(line: String): ListValue {
 }
 
 fun part1(input: List<String>): Int {
-    val listsValue = readSpec(input)
-    return 1
+    val packetList = readSpec(input)
+    return packetList.process()
 }
 
 fun part2(input: List<String>): Int {
@@ -83,7 +185,7 @@ fun part2(input: List<String>): Int {
 
 fun main() {
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day13/Day13_min")
+    val testInput = readInput("Day13/Day13_test")
     println(part1(testInput))
     println(part2(testInput))
 }
